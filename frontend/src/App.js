c08e3413d974dddf5fc6accfb119d6a0902d44a1
Igor Czerwinski <657586+igoratron@users.noreply.github.com>
2020-05-25 13:37:56 +0100

    Query the API for ingredients

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useEffect, useState } from 'react';
 import './App.css';
+
+function searchIngredients(ingredientName) {
+  const apiUrl = "https://mfuqctb1me.execute-api.eu-west-1.amazonaws.com/dev/ingredients";
+  return fetch(`apiUrl?q=${ingredientName}`).then((response) => response.json());
+}
+
+function IngredientSearch() {
+  const [ingredients, setIngredients] = useState([]);
+  const onInputChange = useCallback(event => {
+    searchIngredients(event.currentTarget.value)
+      .then(setIngredients)
+      .catch(() => setIngredients([]));
+  }, []);
+
+  return (
+    <>
+      <input placeholder="Add ingredient" onChange={onInputChange} />
+      <ul className="list">
+        {ingredients.map(ingredient => (
+          <li key={ingredient.id}>
+            <label>
+              <input type="checkbox" value="123" />
+              {ingredient.name}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="two-panes">
+      <div className="two-panes_left">
+        <h2 className="title">Ingredients you have</h2>
+        <ul className="list ingredients">
+          <li>Onion</li>
+        </ul>
+        <IngredientSearch />
+      </div>
+      <div className="two-panes_right">
+        <h2 className="title">Available recipes</h2>
+        <ul className="list">
+          <li>Onion</li>
+        </ul>
+      </div>
     </div>
   );
 }
